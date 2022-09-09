36a476f2920c14d9959e3aefaef49f9e05a530d7
Benjamin Schachter <benschac@gmail.com>
2022-09-09 11:09:23 -0400

    add error flow AddToBalance

--- src/hooks/v2/transactor/AddToBalanceTx.ts
+++ src/hooks/v2/transactor/AddToBalanceTx.ts
@@ -4,32 +4,46 @@ import { V2UserContext } from 'contexts/v2/userContext'
 import { useContext } from 'react'
 
 import { ETH_TOKEN_ADDRESS } from 'constants/v2/juiceboxTokens'
-import { TransactorInstance } from '../../Transactor'
+import invariant from 'tiny-invariant'
+import { onCatch, TransactorInstance } from '../../Transactor'
 
 const DEFAULT_METADATA = 0
 
 export function useAddToBalanceTx(): TransactorInstance<{
   value: BigNumber
 }> {
-  const { transactor, contracts } = useContext(V2UserContext)
+  const { transactor, contracts, version } = useContext(V2UserContext)
   const { projectId } = useContext(V2ProjectContext)
 
   const DEFAULT_MEMO = ''
 
   return ({ value }, txOpts) => {
-    if (!transactor || !projectId || !contracts?.JBETHPaymentTerminal) {
-      txOpts?.onDone?.()
-      return Promise.resolve(false)
-    }
+    try {
+      invariant(transactor && projectId && contracts?.JBETHPaymentTerminal)
+      return transactor(
+        contracts.JBETHPaymentTerminal,
+        'addToBalanceOf',
+        [projectId, value, ETH_TOKEN_ADDRESS, DEFAULT_MEMO, DEFAULT_METADATA],
+        {
+          ...txOpts,
+          value,
+        },
+      )
+    } catch {
+      const missingParam = !transactor
+        ? 'transactor'
+        : !projectId
+        ? 'projectId'
+        : !contracts?.JBETHPaymentTerminal
+        ? 'contracts.JBETHPaymentTerminal'
+        : undefined
 
-    return transactor(
-      contracts?.JBETHPaymentTerminal,
-      'addToBalanceOf',
-      [projectId, value, ETH_TOKEN_ADDRESS, DEFAULT_MEMO, DEFAULT_METADATA],
-      {
-        ...txOpts,
-        value,
-      },
-    )
+      return onCatch({
+        txOpts,
+        missingParam,
+        version,
+        functionName: 'addToBalanceOf',
+      })
+    }
   }
 }
